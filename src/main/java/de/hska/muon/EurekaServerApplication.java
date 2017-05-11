aa61d7bf76d9e4f280f3adae7607b6e229e9d5e7
Gerrit Leuchner <lege1012@IWI-lkit3-11>
2017-05-11 14:55:10 +0200

    eureka implementation

---
+++ src/main/java/de/hska/muon/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package de.hska.muon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
